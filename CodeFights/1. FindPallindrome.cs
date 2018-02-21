6eeccd696b610cffca16053f78f760dd942e53da
tabupl <tabuplaw@gmail.com>
2018-02-21 15:36:24 +0100

    Update 1. FindPallindrome.cs

--- CodeFights/1. FindPallindrome.cs
+++ CodeFights/1. FindPallindrome.cs
@@ -1,4 +1,3 @@
-//Given the string, check if it is a palindrome.
 
 bool checkPalindrome(string inputString)
          {
@@ -9,3 +8,32 @@ bool checkPalindrome(string inputString)
                 }
             return true;
         }
+
+
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+
+*/
